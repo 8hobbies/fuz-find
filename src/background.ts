46fc9b07e861d8be656a7fb6959a843eb33b870d
Hong Xu <hong@8hobbies.com>
2024-11-16 15:09:51 -0800

    Initial commit

---
+++ src/background.ts
@@ -0,0 +1,34 @@
+/** @license GPL-3.0-or-later
+ *
+ * Copyright (C) 2024 8 Hobbies, LLC
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// eslint-disable-next-line @typescript-eslint/no-deprecated
+chrome.runtime.onInstalled.addListener(async (details) => {
+  // eslint-disable-next-line @typescript-eslint/no-deprecated
+  if (details.reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    // eslint-disable-next-line @typescript-eslint/no-deprecated
+    await chrome.tabs.create({
+      url: "https://www.goodaddon.com/fuz-find/",
+    });
+    // eslint-disable-next-line @typescript-eslint/no-deprecated
+  } else if (details.reason === chrome.runtime.OnInstalledReason.UPDATE) {
+    // eslint-disable-next-line @typescript-eslint/no-deprecated
+    await chrome.tabs.create({
+      url: "https://www.goodaddon.com/fuz-find/#changelog",
+    });
+  }
+});
---
+++ vite.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import webExtension, { readJsonFile } from "vite-plugin-web-extension";
+import yearPlugin from "@8hobbies/vite-plugin-year";
+
+function generateManifest() {
+  const manifest = readJsonFile("src/manifest.json");
+  const pkg = readJsonFile("package.json");
+  return {
+    name: pkg.name,
+    description: pkg.description,
+    version: pkg.version,
+    author: pkg.author,
+    homepage_url: pkg.homepage,
+    ...manifest,
+  };
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    minify: false,
+    terserOptions: {
+      compress: false,
+      mangle: false,
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
+  plugins: [
+    react(),
+    webExtension({
+      manifest: generateManifest,
+    }),
+    yearPlugin(),
+  ],
+  test: {
+    environment: "happy-dom",
+    globals: true,
+    setupFiles: ["./vitest-setup.js"],
+  },
+});
